ad4f2e78b8e079f9b279b739c30c3d10ff4fd7cd
subho <subhadeepdeb34@gmail.com>
2020-12-07 17:53:36 +0530

    changed sets to dicts

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -2,10 +2,14 @@
 
 # initialize the valid passport counter
 valid_count = 0
+
+# valid fileds names
+valid_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
+
 # get the inputs from the txt file
 with open('input.txt', 'r') as f:
-    # initialize an empty set to store the required fields
-    fields = set()
+    # initialize an empty dict to store the required fields
+    fields = {i: False for i in valid_fields}
     # read it line by line
     for line in f.readlines():
         # check if the line is empty
@@ -14,16 +18,18 @@ with open('input.txt', 'r') as f:
             # the previous passport
 
             # get the length of the fields list
-            n = len(fields)
-            # if it has 8 fileds then it's valid
+            print(list(fields.values()))
+            n = len(valid_fields) - list(fields.values()).count(False)
+
+            # if it has 8 fields then it's valid
             if n == 8:
                 valid_count += 1
             # if it has 7 fields except for 'cid' then it's also valid
-            elif n == 7 and 'cid' not in fields:
+            elif n == 7 and not fields['cid']:
                 valid_count += 1
 
             # reset the fields list to process the next passport
-            fields = set()
+            fields = {i: False for i in valid_fields}
         # if the line is not empty
         else:
             # get all the key:value pair(s) from the current line
@@ -31,10 +37,11 @@ with open('input.txt', 'r') as f:
             # get the key(s) and append them in the fields list
             for pair in pairs:
                 key, value = pair.strip().split(':')
-                fields.add(key.strip())
+                fields[key.strip()] = value.strip()
 
 # check for the last passport which it not verified in the loop
-if (len(fields) == 8) or (len(fields) == 7 and 'cid' not in fields):
+n = len(valid_fields) - list(fields.values()).count(False)
+if (n == 8) or (n == 7 and not fields['cid']):
     valid_count += 1
 
 print(valid_count)
