609c74c1339e4341afcff49ed958efb1b2bd6d5c
subho <subhadeepdeb34@gmail.com>
2020-12-07 13:33:48 +0530

    first day second puzzle

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -8,7 +8,25 @@ ip_dict = {i: True for i in arr}
 # two numbers that add upto 2020
 for i in arr:
     if ip_dict.get((2020 - i), False) and i != (2020 - i):
-        print(f"{i} * {2020 - i}: {(2020 - i) * i}")
+        # print(f"{i} * {2020 - i}: {(2020 - i) * i}")
+        print((2020 - i) * i)
         break
 
+# make a dictionary that stores the sum of every possible pair and store their
+# product in the value
+pair_dict = {}
+
+for i in range(len(arr) - 1):
+    for j in range(i, len(arr)):
+        pair_dict[arr[i] + arr[j]] = arr[i] * arr[j]
+
 # three numbers that add upto 2020
+for i in arr:
+    # for i not to be present in the (2020-i) sum, (2020-i-i) must not be
+    # present in the input dict
+    if pair_dict.get((2020 - i), False) and \
+        not ip_dict.get((2020 - i - i), False):
+
+        print(pair_dict[2020 - i] * i)
+        break
+
