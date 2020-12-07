f209657b6fbbe3d75d12f2a671909e3518bae368
subho <subhadeepdeb34@gmail.com>
2020-12-07 13:12:25 +0530

    O(n) solution

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -2,9 +2,13 @@
 
 from input import arr
 
+# make a dictionary of the iputs to do a O(1) lookup
+ip_dict = {i: True for i in arr}
+
 # two numbers that add upto 2020
 for i in arr:
-    if (2020 - i) in arr[i:]:
+    if ip_dict.get((2020 - i), False) and i != (2020 - i):
         print(f"{i} * {2020 - i}: {(2020 - i) * i}")
+        break
 
 # three numbers that add upto 2020
