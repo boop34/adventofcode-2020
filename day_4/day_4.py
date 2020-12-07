f80b96c308f4d8c8a11eee232471187a9d7036bd
subho <subhadeepdeb34@gmail.com>
2020-12-07 19:47:20 +0530

    now the function can do both the puzzles

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,96 +1,94 @@
 #!/usr/bin/env python3
 
 # function to check validity
-def check_validity(fields):
+def check_validity(fields, mode=1):
     '''
     fields is a dictionary containg key:value pairs where keys are the valid
     fields and values are the respective information of the fields
 
+    mode -> 1 for the first puzzle
+         -> anything else for the second puzzle :)
+
     returns if the passport is valid or not
     '''
 
-    # check the 'byr'(birth year) field: must be 4 digits and must be within
-    # 1920 to 2002
-    if not fields['byr']:
-        return False
-    try:
-        if int(fields['byr']) < 1920 or int(fields['byr']) > 2002:
+    # for the first puzzle
+    for field, value in fields.items():
+        if not value and field != 'cid':
             return False
-    except ValueError:
-        return False
 
-    # check the 'iyr'(issue year) field: must be 4 digits and must be within
-    # 2010 and 2020
-    if not fields['iyr']:
-        return False
-    try:
-        if int(fields['iyr']) < 2010 or int(fields['iyr']) > 2020:
+    # for the first puzzle the above check is enough
+    if mode == 1:
+        return True
+    else:
+        # check the 'byr'(birth year) field: must be 4 digits and must be
+        # within 1920 to 2002
+        try:
+            if int(fields['byr']) < 1920 or int(fields['byr']) > 2002:
+                return False
+        except ValueError:
             return False
-    except ValueError:
-        return False
 
-    # check the 'eyr'(expiration year) field: must be 4 digits and must be
-    # within 2020 and 2030
-    if not fields['eyr']:
-        return False
-    try:
-        if int(fields['eyr']) < 2020 or int(fields['eyr']) > 2030:
+        # check the 'iyr'(issue year) field: must be 4 digits and must be
+        # within 2010 and 2020
+        try:
+            if int(fields['iyr']) < 2010 or int(fields['iyr']) > 2020:
+                return False
+        except ValueError:
             return False
-    except ValueError:
-        return False
 
-    # check the 'hgt'(height) field: must be a number followed by cm/in
-    # if cm -> the number must be within 150 and 193
-    # in in -> the number must be within 59 and 76
-    # seperate the height and the unit
-    if not fields['hgt']:
-        return False
-    try:
-        h, u = int(fields['hgt'][:-2]), fields['hgt'][-2:]
-    except ValueError:
-        return False
-
-    if u == 'cm' and (h < 150 or h > 193):
-        return False
-    if u == 'in' and (h < 59 or h > 76):
-        return False
-    if u != 'cm' and u != 'in':
-        return False
-
-    # cehck the 'hcl'(hair color) field: must be a '#' followed by 6 chars
-    # between 0-9 or a-f
-    # get the color value, it must be a hex value
-    if not fields['hcl']:
-        return False
-    elif '#' not in fields['hcl']:
-        return False
-    try:
-        hc = int(fields['hcl'][1:], 16)
-        if len(fields['hcl'][1:]) != 6:
+        # check the 'eyr'(expiration year) field: must be 4 digits and must be
+        # within 2020 and 2030
+        try:
+            if int(fields['eyr']) < 2020 or int(fields['eyr']) > 2030:
+                return False
+        except ValueError:
             return False
-    except ValueError:
-        return False
 
-    # check the 'ecl'(eye color) field: must be any one of {'amb', 'blu',
-    # 'brn', 'gry', 'grn', 'hzl', 'oth'}
-    if not fields['ecl']:
-        return False
-    valid_ec = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
-    if fields['ecl'] not in valid_ec:
-        return False
-
-    # check the 'pid'(passport id) field: must be a 9 digit number including
-    # leading 0(s)
-    if not fields['pid']:
-        return False
-    try:
-        if (int(fields['pid']) < 0) or (len(fields['pid']) != 9):
+        # check the 'hgt'(height) field: must be a number followed by cm/in
+        # if cm -> the number must be within 150 and 193
+        # in in -> the number must be within 59 and 76
+        # seperate the height and the unit
+        try:
+            h, u = int(fields['hgt'][:-2]), fields['hgt'][-2:]
+        except ValueError:
             return False
-    except ValueError:
-        return False
 
-    # if passed all the conditions then the passport is valid
-    return True
+        if u == 'cm' and (h < 150 or h > 193):
+            return False
+        if u == 'in' and (h < 59 or h > 76):
+            return False
+        if u != 'cm' and u != 'in':
+            return False
+
+        # cehck the 'hcl'(hair color) field: must be a '#' followed by 6 chars
+        # between 0-9 or a-f
+        # get the color value, it must be a hex value
+        if '#' not in fields['hcl']:
+            return False
+        try:
+            hc = int(fields['hcl'][1:], 16)
+            if len(fields['hcl'][1:]) != 6:
+                return False
+        except ValueError:
+            return False
+
+        # check the 'ecl'(eye color) field: must be any one of {'amb', 'blu',
+        # 'brn', 'gry', 'grn', 'hzl', 'oth'}
+        valid_ec = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        if fields['ecl'] not in valid_ec:
+            return False
+
+        # check the 'pid'(passport id) field: must be a 9 digit number
+        # including leading 0(s)
+        try:
+            if (int(fields['pid']) < 0) or (len(fields['pid']) != 9):
+                return False
+        except ValueError:
+            return False
+
+        # if passed all the conditions then the passport is valid
+        return True
 
 # initialize the valid passport counter
 valid_count = 0
@@ -110,7 +108,7 @@ with open('input.txt', 'r') as f:
             # the previous passport
 
             # if it has 8 fields then it's valid
-            if check_validity(fields):
+            if check_validity(fields, mode=2):
                 valid_count += 1
 
             # reset the fields list to process the next passport
@@ -125,7 +123,7 @@ with open('input.txt', 'r') as f:
                 fields[key.strip()] = value.strip()
 
 # check for the last passport which it not verified in the loop
-if check_validity(fields):
+if check_validity(fields, mode=2):
     valid_count += 1
 
 print(valid_count)
