551ac997cbcdd46c66ef459dc5ac41ee2028dab7
subho <subhadeepdeb34@gmail.com>
2020-12-10 14:31:52 +0530

    day ten second puzzle

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -68,3 +68,38 @@ diff = get_difference(arr)
 # for the first puzzle
 print(diff[1] * diff[3])
 
+# function to find the total number of ways to arrange the adapters
+def arrange_adapters(arr):
+    '''
+    this function takes an array(arr) of adapter jolt ratings
+
+    it returns the number of distinct ways to arrange the adapters
+    '''
+    # make a dictionary to store all the possible ways to arrange the adapters
+    # to make the jolt rating possible
+    jolts = {i: True for i in arr}
+
+    # include the device adapter jolt into the dict
+    device_jolt = max(arr) + 3
+    jolts[device_jolt] = -1
+
+    # initialize the number of eays to arrange the first 3 jolts
+    jolts[1] = 1    # as there is only one way to make a 1 jolt output -> (0, 1)
+    jolts[2] = 2    # as there is only one way to make a 2 jolt output -> (0, 1, 2), (0, 2)
+    jolts[3] = 4    # as there are 2 ways to make a 3 jolt output -> (0, 1, 2, 3), (0, 1, 3), (0, 2, 3), (0, 3)
+
+    # now check for the other jolts derived from the base three jolts
+    for i in range(4, device_jolt + 1):
+        # if the jolt is not present in the bags or device ignore it
+        if not jolts.get(i, False):
+            continue
+        # if the jolt is present
+        else:
+            # add up the previous 3 ways to get to current jolt
+            jolts[i] = jolts.get(i - 1, 0) + jolts.get(i - 2, 0) + jolts.get(i - 3, 0)
+
+    # return the maximum number of ways to output device_jolt
+    return jolts[device_jolt]
+
+# for the second puzzle
+print(arrange_adapters(arr))
