8425a028adf9e8926eee29f40ff51857e64aafa9
subho <subhadeepdeb34@gmail.com>
2020-12-17 14:13:18 +0530

    day fourteen second puzzle

--- day_14/day_14.py
+++ day_14/day_14.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import itertools
+
 # function to apply bitmask
-def apply_mask(mask, num):
+def apply_mask1(mask, num):
     '''
     this function takes a mask of 36 characters and pllies that to the integer
     num
@@ -30,8 +32,72 @@ def apply_mask(mask, num):
     return int(''.join(b), 2)
 
 
+# function to apply bitmask for the second puzzle
+def apply_mask2(mask, num):
+    '''
+    this function takes a mask of 36 characters and pllies that to the integer
+    num
+
+    it returns a integer after appling the mask
+    '''
+    # convert the input integer into a binary of 36 length with 0 padding
+    b = [i for i in f'{int(num):036b}']
+
+    # get the bits from the mask to change
+    bit_mask = {}
+
+    # populate the bit mask
+    for i, v in enumerate(mask):
+        bit_mask[i] = v
+
+    # apply the bit mask
+    for idx in bit_mask:
+        # If the bitmask bit is 0, the corresponding memory address bit
+        # is unchanged
+        if bit_mask[idx] == '0':
+            continue
+        # If the bitmask bit is 1, the corresponding memory address bit
+        # is overwritten with 1
+        # If the bitmask bit is X, the corresponding memory address bit
+        # is floating
+        else:
+            b[idx] = bit_mask[idx]
+
+    # number of floating bits
+    n = b.count('X')
+
+    # generate every possible combination for n number of floating bits
+    combinations = list(itertools.product([0, 1], repeat=n))
+
+    # initialize a list to store all the possible numbers generated after
+    # handling floating bits
+    nums = []
+
+    # iterate over every combination
+    for combination in combinations:
+        # make a copy of the original masked number
+        b_tmp = b.copy()
+        # iterate over the individual values of the combination
+        for i in combination:
+            # now check the number
+            for j in range(len(b_tmp)):
+                # if a floating bit is found
+                if b_tmp[j] == 'X':
+                    # replace it with the combination bit
+                    b_tmp[j] = str(i)
+                    break
+        # after all the floating bits are replaced
+        nums.append(int(''.join(b_tmp), 2))
+
+    # return the list of possible numbers
+    return nums
+
+
+
 # initialize a memory dictionary to store the value at certain address
-memory = {}
+memory1 = {}
+# initialize a memory dictionary to store the value at certain address
+memory2 = {}
 
 # fetch the input
 with open('input.txt', 'r') as f:
@@ -48,6 +114,16 @@ with open('input.txt', 'r') as f:
             # get the address
             addr = int(r.strip().split('[')[1][:-1])
             # fill the memory address by the number after appling the mask
-            memory[addr] = apply_mask(mask, l.strip())
+            memory1[addr] = apply_mask1(mask, l.strip())
+            # get the posswible memory addresses for second puzzle
+            nums = apply_mask2(mask, addr)
+            # now fill memory2 with the addresses
+            for num in nums:
+                # fill the address with the value
+                memory2[num] = int(l.strip())
 
-print(sum(memory.values()))
+# for the first puzzle
+print(sum(memory1.values()))
+
+# for the second puzzle
+print(sum(memory2.values()))
