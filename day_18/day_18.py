40e12da7b3d48c998d0d8d32f35947fd9e85caca
subho <subhadeepdeb34@gmail.com>
2020-12-18 14:11:59 +0530

    day eighteen second puzzle

--- day_18/day_18.py
+++ day_18/day_18.py
@@ -15,7 +15,8 @@ class MyNumber:
         return f'MyNumber({self.val})'
 
     # NOTE: the addition(+) and the subtraction(-) operators have the same
-    # precedence that's why they are the one overloaded
+    # precedence and bitwise AND(&) operator has lower precedence than them
+    # that's why they are the one overloaded
 
     # if multiplication(*) operator was overloaded it wouldn't have effected
     # the resultant expression as in the time of parsing the python
@@ -36,16 +37,26 @@ class MyNumber:
 
         return MyNumber(f'({self.val} * {other.val})')
 
+    # overload the bitwise AND operator with multiplication repressentation
+    # for the second part of the puzzle
+    def __and__(self, other):
+        # check if the other is a MyNumber object
+        assert type(other) == MyNumber, "Other should be MyNumber object"
 
-# initialize a variable to store the summation
-s = 0
+        return MyNumber(f'({self.val} * {other.val})')
+
+
+# initialize variables to store the summation
+s1 = 0
+s2 = 0
 
 # fetch the input
 with open('input.txt', 'r') as f:
     # parse the input line by line
     for line in f.readlines():
-        # initialize an empty list to store all the elements
-        exp_list = []
+        # initialize emptys list to store all the elements
+        exp_list1 = []
+        exp_list2 = []
         # parse the current line
         for ele in line.strip():
             # if the current element is a whitespace
@@ -56,25 +67,34 @@ with open('input.txt', 'r') as f:
             elif ele.isnumeric():
                 # make it an object of the custom MyNumber class and append it
                 # into the list
-                exp_list.append(str(MyNumber(int(ele))))
+                exp_list1.append(str(MyNumber(int(ele))))
+                exp_list2.append(str(MyNumber(int(ele))))
             # if it's a '*'
             elif ele == '*':
                 # change it to '-'
-                exp_list.append('-')
+                exp_list1.append('-')
+                exp_list2.append('&')
             # otherwise it's a valid element
             else:
                 # append the elemet to the list
-                exp_list.append(ele)
+                exp_list1.append(ele)
+                exp_list2.append(ele)
 
         # create the expression string by joining the elements of the list
         # then call eval on it to evaluate
-        exp = eval(''.join(exp_list))
+        exp1 = eval(''.join(exp_list1))
+        exp2 = eval(''.join(exp_list2))
         # now that we have exp as an MyNumber object we can call eval on it's
         # val to get the evaluation of the original expression
-        value = eval(exp.val)
+        value1 = eval(exp1.val)
+        value2 = eval(exp2.val)
 
         # add the value to the sum
-        s += value
+        s1 += value1
+        s2 += value2
 
 # for the first puzzle
-print(s)
+print(s1)
+
+# for the second puzzle
+print(s2)
